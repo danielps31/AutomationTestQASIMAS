bdb5d551bb73575c5a7a210000316854812368ea
Dimas Gunawan <81980945+FungiWhy@users.noreply.github.com>
2022-11-01 11:49:30 +0700

    (EDIT REGISTER FORM)

--- Scripts/KB/Register Form/Script1667226530286.groovy
+++ Scripts/KB/Register Form/Script1667226530286.groovy
@@ -29,13 +29,9 @@ WebUI.setText(findTestObject('Object Repository/Page_Pega Platform/input_Passwor
 
 WebUI.click(findTestObject('Object Repository/Page_Pega Platform/button_Log in'))
 
-WebUI.delay(10)
-
 WebUI.click(findTestObject('Object Repository/Page_Case Worker/button_Formulir Klaim'))
 
-WebUI.delay(5)
-
-WebUI.switchToWindowTitle('Formulir Claim FRM-9702')
+WebUI.switchToWindowIndex(1)
 
 WebUI.maximizeWindow()
 
@@ -51,9 +47,13 @@ WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/butto
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
 
+WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
+
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/a_31'))
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/img_Tanggal Kejadian_CalendarImg-0509d02b'))
+
+WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
 
@@ -102,41 +102,37 @@ WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/butto
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Upload'))
 
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\FORMUPLOAD.jpg')
+WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'Assets/File Upload/Dokumen/Foto Formulir klaim.png')
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit'))
 
 WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/select_Pilih StatusRegister KlaimBatal Klai_f1b285'), 
     'Register Klaim', true)
 
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Simpan_1'))
-
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Lanjut'))
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Upload All Document'))
 
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\KTP.jpg')
+WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'Assets/File Upload/Dokumen/Foto Formulir klaim.png')
 
 WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/select_Pilih DokumenKTPSTNK'), 
     '2209120021473155', true)
 
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\STNK.jpg')
+WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'Assets/File Upload/Dokumen/Foto Formulir klaim.png')
 
 WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/select_Pilih DokumenKTPSTNK_1'), 
     '2209120021473157', true)
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Submit'))
 
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Simpan'))
-
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/button_Unggah'))
 
-WebUI.uploadFile(findTestObject('Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\Belakang.jpg')
+WebUI.uploadFile(findTestObject('Page_Formulir/UploadForm'), 'Assets/File Upload/Dokumen/Foto Formulir klaim.png')
 
 WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/select_--- Pilih Sisi Foto ---Tampak DepanT_1a682b'), 
     'Tampak Belakang', true)
 
-WebUI.uploadFile(findTestObject('Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\Depan.jpg')
+WebUI.uploadFile(findTestObject('Page_Formulir/UploadForm'), 'Assets/File Upload/Dokumen/Foto Formulir klaim.png')
 
 WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/select_--- Pilih Sisi Foto ---Tampak DepanT_1a682b_1'), 
     'Tampak Depan', true)
@@ -144,138 +140,4 @@ WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/button_Submit'))
 
 WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/button_Simpan'))
-
-WebUI.closeBrowser()
-
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://apptest.claimq.id/prweb/tHGV_g6FQZMSYakWUervaQ%28%28*/!STANDARD')
-
-WebUI.maximizeWindow()
-
-WebUI.setText(findTestObject('Object Repository/Page_Pega Platform/input_User name_UserIdentifier'), 'testingsurveyor@asm2')
-
-WebUI.setText(findTestObject('Object Repository/Page_Pega Platform/input_Password_Password'), '123456')
-
-WebUI.click(findTestObject('Object Repository/Page_Pega Platform/button_Log in'))
-
-WebUI.delay(10)
-
-WebUI.click(findTestObject('Object Repository/Page_Case Worker/button_Formulir Klaim'))
-
-WebUI.delay(5)
-
-WebUI.switchToWindowTitle('Formulir Claim FRM-9703')
-
-WebUI.maximizeWindow()
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/img_Tanggal Lapor_CalendarImg-0079ae87'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/a_31'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/img_Tanggal Kejadian_CalendarImg-0509d02b'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim_spin-bu_7c04b7'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/a_25'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/input_Required_PWorkFormpPolisNo'), '12100000241293')
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Ambil Data Polis'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Simpan'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/input_No Plat Kendaraan_PWorkFormpPolisNoPlat'), 
-    'B1310TTN')
-
-WebUI.setText(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/input_No Mesin_PWorkFormpPolisNoMesin'), 'K148T1088075TTN')
-
-WebUI.setText(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/input_No Rangka_PWorkFormpPolisNoRangka'), 
-    'MHYKZE81SDJ228149TTN')
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/select_-Pilih Salah Satu-Kerusakan Sebagian_d89ffd'), 
-    '1', true)
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/select_-Pilih Salah Satu-Own Damage (OD)TJH'), 
-    'OD', true)
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/label_Ya'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/input_Wilayah Klaim_PWorkFormpWilayahKlaim'), 
-    'TESTING ASM')
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/span_TESTING ASM'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/input_Surveyor_PpyWorkPagepIdSurveyor'), 'TESTINGSURVE')
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/span_TESTINGSURVEYORASM2'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit  Cetak Formulir Klaim'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Upload'))
-
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\FORMUPLOAD.jpg')
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Submit'))
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/select_Pilih StatusRegister KlaimBatal Klai_f1b285'), 
-    'Register Klaim', true)
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9692/button_Simpan_1'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Lanjut'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Upload All Document'))
-
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\KTP.jpg')
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/select_Pilih DokumenKTPSTNK'), 
-    '2209120021473155', true)
-
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\STNK.jpg')
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/select_Pilih DokumenKTPSTNK_1'), 
-    '2209120021473157', true)
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Submit'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9697/button_Simpan'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/button_Unggah'))
-
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\Belakang.jpg')
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/select_--- Pilih Sisi Foto ---Tampak DepanT_1a682b'), 
-    'Tampak Belakang', true)
-
-WebUI.uploadFile(findTestObject('Object Repository/Page_Formulir/UploadForm'), 'G:\\QA\\Gambar\\Depan.jpg')
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/select_--- Pilih Sisi Foto ---Tampak DepanT_1a682b_1'), 
-    'Tampak Depan', true)
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/button_Submit'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9700/button_Simpan'))
-
-WebUI.click(findTestObject('Object Repository/Page_Formulir Claim FRM-9703/button_Submit'))
-
-WebUI.navigateToUrl('https://apptest.claimq.id/prweb/hYbtfX1gnQLb4rCVZ6dGog%28%28*/!STANDARD?pyActivity=FinishAssignment&pzFromFrame=pyWorkPage&pzPrimaryPageName=pyWorkPage&pzTransactionId=d54fbf281461392bec43d37703cb96b3&pzHarnessID=HID10880F94AB52BFDD8FC1A1C67E687773')
 
